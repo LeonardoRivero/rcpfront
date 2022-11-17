a116c174dee562c6dd8cdf2911d4940da50d45cf
Leonardo Rivero <lriveroarenas@gmail.com>
2022-11-17 17:47:51 -0500

    se modifica quasar config para probar subir archivos estaticos

--- quasar.config.js
+++ quasar.config.js
@@ -68,7 +68,7 @@ module.exports = configure(function (ctx) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      distDir: ctx.mode.spa ? 'public' : null,
+      // distDir: ctx.mode.spa ? 'public' : null,
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
